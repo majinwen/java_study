4cefcb6ca0c4f3c6f548e7cec21c3319bdef565f
pengyao1207 <30586004+pengyao1207@users.noreply.github.com>
2018-01-27 09:51:03 +0800

    3

--- GK培训/作业/第三天作业/java_class1/src/Student.java
+++ GK培训/作业/第三天作业/java_class1/src/Student.java
@@ -1,53 +1,26 @@
 public class Student {
     private String name;
     private int age;
-    private int cls;//班级
+
+    static int cls = 1;//班级
+
     private Score score;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    public void setName(String name) { this.name = name; }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+    public void setAge(int age) { this.age = age; }
 
-    public void setCls(int cls) {
-        this.cls = cls;
-    }
+    public void setScore(Score score) { this.score = score; }
 
-    public void setScore(Score score) {
-        this.score = score;
-    }
+    public String getName() { return name; }
 
-    public String getName() {
-        return name;
-    }
+    public int getAge() { return age; }
 
-    public int getAge() {
-        return age;
-    }
+    public Score getScore() { return score; }
 
-    public int getCls() {
-        return cls;
-    }
+    Student() { name = "默认"; age = 0;cls = 0;score = new Score(); }
 
-    public Score getScore() {
-        return score;
-    }
-    Student(){
-        name="默认";
-        age=0;
-        cls=0;
-        score=new Score();
-    }
-    Student(String name,int age,int cls,Score score){
-        this.name=name;
-        this.age=age;
-        this.cls=cls;
-        this.score=score;
-    }
-    public void speak(){
-        System.out.println("说中文。。。");
-    }
+    Student(String name, int age, int cls, Score score) { this.name = name;this.age = age;     this.cls = cls;this.score = score; }
+
+    public static  void speak() { System.out.println("说中文。。。"); }
 }
--- GK培训/作业/第三天作业/java_class1/src/Test2.java
+++ GK培训/作业/第三天作业/java_class1/src/Test2.java
@@ -1,9 +1,32 @@
 public class Test2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         Score score = new Score();
         score.setChinese(88);
-        Student student = new Student("pengyao1207",20,4,score);
+        Student student = new Student("pengyao1207", 20, 4, score);
+
+        //....打印结果是88，没啥问题
         System.out.println(student.getScore().getChinese());
-        student.speak();
+
+
+        //打印结果是77，这说明是score和student中的score仍然有关联，如何解决？
+        //如何切断这层关联？
+        //也就是说，如何拷贝一个对象,以何种方式拷贝比较合适？
+        score.setChinese(77);
+        System.out.println(student.getScore().getChinese());
+
+        //打印结果是66，也就是说，即使没有方法setScore,我也能设置成绩
+        //即使Student中的Score加了private属性，依旧。。。如何解决？
+        student.getScore().setChinese(66);
+        System.out.println(student.getScore().getChinese());
+
+
+        //另外,我对String类型的name做了同样的测试，上述问题对String不存在
+        //也就是说，Score类和String类有区别，什么区别?
+
+
+// 静态的测试，ok
+//       Student.cls=6;
+//        Student.speak();
     }
 }
